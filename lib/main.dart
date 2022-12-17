3e8d337e3ce4fe3963e7666ffc618358bc6ef866
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-12-17 12:58:18 -0300

    lists and buttons list added

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,34 @@ class QuestionApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final questions = [
+      'Qual o nome do "cachorro" do bob esponja?',
+      'Qual o nome do filho do darth vader?'
+    ];
+    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Center(child: Text('Perguntas')),
         ),
-        body: const Text('Hello Flutter!!!'),
-      ),
+        body: Column(
+          children: <Widget>[
+            Text(questions[0]),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 1'),
+            ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 2'),
+            ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 3'),
+            )
+          ],
+        )
+        ),
     );
   }
 }
