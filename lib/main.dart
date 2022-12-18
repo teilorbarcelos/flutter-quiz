4b5108ca0a56c1a5d2c9a8a57692402104bf2543
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-12-18 20:54:12 -0300

    out of range question bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -6,46 +6,53 @@ void main(List<String> args) => runApp(const QuestionApp());
 
 class _QuestionAppState extends State<QuestionApp> {
   var _selectedQuestion = 0;
+  final List<Map<String, Object>> _questions = [
+    {
+      'text': 'Qual o nome do "cachorro" do bob esponja?',
+      'answers': [
+        'Pluto',
+        'Patriky',
+        'Garry',
+        'Plankton',
+      ]
+    },
+    {
+      'text': 'Qual o nome do filho do darth vader?',
+      'answers': [
+        'Lando',
+        'Luke',
+        'Gandalf',
+        'Chewbaka',
+      ]
+    },
+    {
+      'text': 'Qual a cor do cavalo branco de Napoleão?',
+      'answers': [
+        'marrom',
+        'branco',
+        'Preto',
+        'branca',
+      ]
+    }
+  ];
 
   void _answer() {
-    setState(() {
-      _selectedQuestion++;
-    });
+    if (hasSelectedQuestion) {
+      setState(() {
+        _selectedQuestion++;
+      });
+    }
+  }
+
+  bool get hasSelectedQuestion {
+    return _selectedQuestion < _questions.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, Object>> questions = [
-      {
-        'text': 'Qual o nome do "cachorro" do bob esponja?',
-        'answers': [
-          'Pluto',
-          'Patriky',
-          'Garry',
-          'Plankton',
-        ]
-      },
-      {
-        'text': 'Qual o nome do filho do darth vader?',
-        'answers': [
-          'Lando',
-          'Luke',
-          'Gandalf',
-          'Chewbaka',
-        ]
-      },
-      {
-        'text': 'Qual a cor do cavalo branco de Napoleão?',
-        'answers': [
-          'marrom',
-          'branco',
-          'Preto',
-          'branca',
-        ]
-      }
-    ];
-
-    List<String> answers = questions[_selectedQuestion].cast()['answers'];
+    List<String> answers = hasSelectedQuestion
+        ? _questions[_selectedQuestion].cast()['answers']
+        : [];
     List<Widget> answersList = answers
         .map((textAnswer) => AnswerOption(text: textAnswer, onSelect: _answer))
         .toList();
@@ -56,10 +63,12 @@ class _QuestionAppState extends State<QuestionApp> {
             title: const Center(child: Text('Perguntas')),
           ),
           body: Column(
-            children: <Widget>[
-              Question(questions[_selectedQuestion]['text'].toString()),
-              ...answersList
-            ],
+            children: hasSelectedQuestion
+                ? <Widget>[
+                    Question(_questions[_selectedQuestion]['text'].toString()),
+                    ...answersList
+                  ]
+                : [],
           )),
     );
   }
